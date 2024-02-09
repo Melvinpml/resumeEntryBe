6967cee68827e8a4057cbc0fecaf6da40295919a
Melvinpml <melvinpoulose06@gmail.com>
2024-02-09 16:16:43 +0530

    updtd signup

--- controller/signupRouter.js
+++ controller/signupRouter.js
@@ -5,23 +5,49 @@ const bcrypt = require("bcryptjs")
 
 const router = express.Router()
 
-hashPassGen = async(pass)=>{
+hashPassGen = async (pass) => {
     const salt = await bcrypt.genSalt(10)
-    return bcrypt.hash(pass,salt)
+    return bcrypt.hash(pass, salt)
 }
 
-router.post("/add",async(req,res)=>{
-    let {data} = {"data" : req.body}
+router.post("/add", async (req, res) => {
+    let { data } = { "data": req.body }
     let password = data.password
     hashPassGen(password).then(
-        (hashPass)=>{
+        (hashPass) => {
             data.password = hashPass
             let signup = new signupModel(data)
             let result = signup.save()
+            res.json({ status: "success" })
             console.log(data)
         }
     )
 
+})
+
+router.post("/signin", async (req, res) => {
+    let input = req.body
+    let email = req.body.email
+    let data = await signupModel.findOne({ "email": email })
+    if (!data) {
+        return res.json({
+            status: "invalid user"
+        })
+    }
+    console.log(data)
+    let dbpassword = data.password
+    let inputpassword = req.body.password
+    console.log(dbpassword)
+    console.log(inputpassword)
+    const match = await bcrypt.compare(inputpassword, dbpassword)
+    if (!match) {
+        return res.json({
+            status: "incorrect password"
+        })
+    }
+    res.json({
+        status: "success"
+    })
 })
 
 module.exports = router
